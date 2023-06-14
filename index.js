5340bec62324903d58f5981d8456098eaeb96c40
+ <+>
2023-06-13 21:09:51 -0400

    DOM content

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('After DOM Loaded')
+    document.body.innerHTML = document.body.innerHTML.replace('JavaScript is so cool. It lets me add text to my page programmatically.', 'This is really cool!')
+})
+console.log('Before DOM Loads')
